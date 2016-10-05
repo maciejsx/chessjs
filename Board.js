1522e6048ec80e4b53d963b4253fb86f9b94982b
m <m@exia.io>
2016-10-05 22:34:27 +0200

    change concept

---
+++ Board.js
@@ -0,0 +1,15 @@
+var board = new Board();
+
+function Board() {
+    var ROW_SIZE = 8, COLUMN_SIZE = 8;
+    var public = {};
+    var field = {};
+
+    public.field = field;
+
+    function () {
+
+    }
+
+
+}
--- main.js
+++ main.js
@@ -1,22 +1,29 @@
+
 var ROW_SIZE = 8;
 
 function generateField() {
     $('.field').append('<div class="board"></div>');
-
-    var columnName;
-
-    var column = 0;
-
     for (var row = 0; row < ROW_SIZE; row++) {
         $('.board').append('<div class="row" id="row' + row + '"></div>');
-        for (column = 0; column < ROW_SIZE; column++) {
+        for (var column = 0; column < ROW_SIZE; column++) {
             $('#row' + row).append('<div class="column" id="column' + row + column + '"></div>');
-            console.log(row + column);
             if ((row + column) % 2 !== 0) {
                 $('#column' + row + column).css('background-color', 'black');
             }
         }
     }
+}
+
+function refreshFielsd() {
+
+}
+
+function addListeners() {
+
+}
+
+function init() {
+
 }
 
 $(document).ready(function() {

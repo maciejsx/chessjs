7b3fbf78a6cb777f84c5c1cb45f8265b30c7f973
maciejsx <m@exia.io>
2016-10-05 15:58:23 +0200

    init#2

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-
-function createTableHeader() {
-
-}
+var ROW_SIZE = 8;
 
 function generateField() {
-    $('.field').append( '<table class=".table"></table>')
-    for()
+    $('.field').append('<div class="board"></div>');
+
+    var columnName;
+
+    var column = 0;
+
+    for (var row = 0; row < ROW_SIZE; row++) {
+        $('.board').append('<div class="row" id="row' + row + '"></div>');
+        for (column = 0; column < ROW_SIZE; column++) {
+            $('#row' + row).append('<div class="column" id="column' + row + column + '"></div>');
+            console.log(row + column);
+            if ((row + column) % 2 === 0) {
+                $('#column' + row + column).css('background-color', 'black');
+            }
+        }
+    }
 }
 
-function parseDateFromString(stringDate) {
-    return new Date(Date.parse(stringDate));
-}
-
-$(document).ready(function(){
-    $('.a').on('click', function () {
-       console.log('asd');
-    });
-    $('.b').on('click', function () {
-        console.log('ddd');
-    });
-    console.log(parseDateFromString("2015"));
+$(document).ready(function() {
     $('.start').on('click', function () {
-        $('.field').css('background-color', 'red');
+        generateField();
     });
-
 });
